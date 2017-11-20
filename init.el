a1f1b6b899740f35b4dfcb1608907f44768dac88
Yuki Nagae <y-nagae@aptpod.co.jp>
2017-11-20 12:36:17 +0900

    add version-control showing diff settings

--- init.el
+++ init.el
@@ -55,6 +55,10 @@ values."
      spell-checking
      syntax-checking
      version-control
+     (version-control :variables
+                      version-control-diff-tool 'diff-hl
+                      version-control-diff-side 'left
+                      version-control-global-margin t)
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
